4650c39f820af49e40fd2145e5e575bc19e34099
KatyaKubar <116179784+KatyaKubar@users.noreply.github.com>
2024-02-07 15:37:37 +0200

    Summary

---
+++ src/js/2-snackbar.js
@@ -0,0 +1,45 @@
+// Описаний у документації
+import iziToast from 'izitoast';
+// Додатковий імпорт стилів
+import 'izitoast/dist/css/iziToast.min.css';
+
+const delayInput = document.querySelector('input[name="delay"]');
+const stateRadioButtons = document.querySelectorAll('input[name="state"]');
+
+document.addEventListener('DOMContentLoaded', function () {
+  document.querySelector('form').addEventListener('submit', function (event) {
+    event.preventDefault();
+
+    const delay = parseInt(delayInput.value, 10);
+
+    const selectedState = Array.from(stateRadioButtons).find(
+      input => input.checked
+    ).value;
+
+    const promise = new Promise((resolve, reject) => {
+      setTimeout(() => {
+        if (selectedState === 'fulfilled') {
+          resolve(delay);
+        } else {
+          reject(delay);
+        }
+      }, delay);
+    });
+
+    promise
+      .then(result => {
+        iziToast.success({
+          title: 'Fulfilled Promise',
+          message: `✅ Fulfilled promise in ${result}ms`,
+          position: 'topCenter',
+        });
+      })
+      .catch(error => {
+        iziToast.error({
+          title: 'Rejected Promise',
+          message: `❌ Rejected promise in ${error}ms`,
+          position: 'topCenter',
+        });
+      });
+  });
+});
